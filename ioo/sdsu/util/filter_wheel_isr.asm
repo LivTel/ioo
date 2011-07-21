3150d80153cedf439b0d24d2f24e1f7ead842525
Chris Mottram <C.Mottram@ljmu.ac.uk>
2011-07-21 10:51:03 +0000

    First O version, with clutch engage/disengage and too many locator outputs. This ISR version did not wotk and caused FWM commands to return ERR and not set variables.

--- ioo/sdsu/util/filter_wheel_isr.asm
+++ ioo/sdsu/util/filter_wheel_isr.asm
@@ -1,76 +1,207 @@
 ; filter_wheel_isr.asm
        COMMENT * 
-	$Header: /space/home/eng/cjm/cvs/ioo/sdsu/util/filter_wheel_isr.asm,v 1.1 2011-06-02 14:06:08 cjm Exp $
+	$Header: /space/home/eng/cjm/cvs/ioo/sdsu/util/filter_wheel_isr.asm,v 1.2 2011-07-21 10:51:03 cjm Exp $
 	This include file is called from the 0.8ms SERVICE routine.
 	It evaluates the digital inputs DIG_IN, and may set digital outputs DIG_OUT.
 	It runs only if we are currently doing a filter wheel operation.
 	This source file should be included in utilappl.asm, near "UPD_DIG". Ideally, it should
 	come after the digital inputs are moved from RD_DIG to DIG_IN, but before
 	the outputs are moved from DIG_OUT to WR_DIG.
-	Version: $Revision: 1.1 $
+	Version: $Revision: 1.2 $
 	Author: $Author: cjm $
 	*
 
 ; Filter wheel move
-	JCLR    #ST_FW_MOVE,X:<STATUS,FW_ISR_FWR 	; If the move bit in status is clear, go to FW_ISR_FWR
-; FW_DETENT_CHECK_NUM = (FW_DETENT_CHECK_NUM+1)
-	MOVE	Y:<FW_DETENT_CHECK_NUM,A		; A = FW_DETENT_CHECK_NUM
+	JCLR    #ST_FW_MOVE,X:<STATUS,FW_ISR_END 	; If the move bit in status is clear, go to FW_ISR_END
+; We are in a filer wheel move operation
+; If we are not engaging the clutch goto FW_ISR_LOCATORS_OUT
+	JCLR	#ST_FW_ENGAGE_CLUTCH,X:<STATUS,FW_ISR_LOCATORS_OUT 
+; we are engaging the clutch at this point
+	BSET	#DIG_OUT_BIT_CLUTCH_ENGAGE,Y:<DIG_OUT ; set engage clutch on Y:DIG_OUT
+; Have we engaged the clutch yet
+; if clutch engaged goto FW_ISR_ENGAGE_CLUTCH_DONE
+	JSET	#DIG_IN_BIT_CLUTCH_ENGAGED,Y:<DIG_IN,FW_ISR_ENGAGE_CLUTCH_DONE 
+; FW_MOVE_TIMEOUT = (FW_MOVE_TIMEOUT+1)
+	MOVE	Y:<FW_MOVE_TIMEOUT,A			; A = FW_MOVE_TIMEOUT
 	MOVE	X:<ONE,X0				; X0 = 1
-	ADD	X0,A					; A = FW_DETENT_CHECK_NUM+1
-	MOVE	A,Y:<FW_DETENT_CHECK_NUM		; FW_DETENT_CHECK_NUM = A (FW_DETENT_CHECK_NUM+1)
-; if FW_DETENT_CHECK_NUM >= FW_DETENT_CHECK_COUNT enable detent checking
-	MOVE	Y:<FW_DETENT_CHECK_NUM,A		; A = FW_DETENT_CHECK_NUM
-	MOVE	Y:<FW_DETENT_CHECK_COUNT,X0		; X0 = FW_DETENT_CHECK_COUNT
-	CMP	X0,A					; Is FW_DETENT_CHECK_NUM < FW_DETENT_CHECK_COUNT?
-	JLT	<FW_ISR_END				; If (FW_DETENT_CHECK_NUM < FW_DETENT_CHECK_COUNT) 
-							; 	goto FW_ISR_END
-	BSET	#ST_FW_DETENT_CHECK,X:<STATUS		; We are detent checking
-; wheel 0
-; check whether we are in a detent or not and jump accordingly
-	JSET	#FW0_INPUT_DETENT,Y:<DIG_IN,FW_ISR_FWM_IN_DETENT ; If the detent bit in Y:DIG_IN is set,
-							; goto FW_ISR_FWM_IN_DETENT.
-							; If the bit is set the wheel is in a detent.
-	JMP	<FW_ISR_FWM_OUT_DETENT			; Go to FW_ISR_FWM_OUT_DETENT (end of FW ISR)
-FW_ISR_FWM_IN_DETENT
-	JSET	#ST_FW_IN_DETENT,X:<STATUS,FW_ISR_END 	; If the status detent bit is set,
-							; goto end of ISR (no detent state change).
-	BSET	#ST_FW_IN_DETENT,X:<STATUS		; Set ST_FW_IN_DETENT status bit.
-; reset FW_DETENT_CHECK_NUM to zero
+	ADD	X0,A					; A = FW_MOVE_TIMEOUT+1
+	MOVE	A,Y:<FW_MOVE_TIMEOUT			; FW_MOVE_TIMEOUT = A (FW_MOVE_TIMEOUT+1)
+; if FW_MOVE_TIMEOUT >= #100000 move timeout
+	MOVE	#>100000,X0
+	CMP	X0,A					; Is FW_MOVE_TIMEOUT < #100000
+	JLT	<FW_ISR_END				; If (FW_MOVE_TIMEOUT < #100000) goto FW_ISR_END
+; timeout error
+	BCLR    #ST_FW_MOVE,X:<STATUS			; Take out of filter wheel move mode.
+	BCLR    #ST_FW_ENGAGE_CLUTCH,X:<STATUS		; We are no longer engaging the clutch
+	MOVE	#>5,X0					; X0 = 5
+	MOVE	X0,Y:<FW_ERROR_CODE			; Y:<FW_ERROR_CODE = X0 = 5
+	JMP	<FW_ISR_END
+FW_ISR_ENGAGE_CLUTCH_DONE
+	BCLR    #ST_FW_ENGAGE_CLUTCH,X:<STATUS		; We have finished engaging the clutch
+	BSET    #ST_FW_LOCATORS_OUT,X:<STATUS		; Start moving the locators out
+; reset move timeout FW_MOVE_TIMEOUT to zero
 	MOVE	X:<ZERO,X0				; X0 = 0
-	MOVE	X0,Y:<FW_DETENT_CHECK_NUM		; FW_DETENT_CHECK_NUM = 0
-	BCLR	#ST_FW_DETENT_CHECK,X:<STATUS		; We are not detent checking - wait until out of detent.
-; FW_POS_MOVE--
-	MOVE	X:<ZERO,A				; A = 0 - test for CMP below (ie A1 also 0)
-	MOVE	Y:<FW_POS_MOVE,A0			; A = FW_POS_MOVE
-	DEC	A					; A-- -> FW_POS_MOVE--
-	MOVE 	A0,Y:<FW_POS_MOVE			; FW_POS_MOVE = A e.g. FW_POS_MOVE--
+	MOVE	X0,Y:<FW_MOVE_TIMEOUT			; FW_MOVE_TIMEOUT = 0
+	JMP	<FW_ISR_END
+FW_ISR_LOCATORS_OUT
+; If we are not moving the locators out goto FW_ISR_READING_PROXIMITY
+	JCLR	#ST_FW_LOCATORS_OUT,X:<STATUS,FW_ISR_READING_PROXIMITY 
+; set digital output bits to move locators out
+	BCLR	#DIG_OUT_BIT_LOCATOR_1_IN,Y:<DIG_OUT	; move locator 1 out
+	BCLR	#DIG_OUT_BIT_LOCATOR_2_IN,Y:<DIG_OUT	; move locator 2 out
+	BCLR	#DIG_OUT_BIT_LOCATOR_3_IN,Y:<DIG_OUT	; move locator 3 out
+; check whether the locators are out
+; if locator 1 not out goto FW_ISR_LOCATOR_NOT_OUT
+	JCLR	#DIG_IN_BIT_LOCATOR_1_OUT,Y:<DIG_IN,FW_ISR_LOCATOR_NOT_OUT
+; if locator 2 not out goto FW_ISR_LOCATOR_NOT_OUT
+	JCLR	#DIG_IN_BIT_LOCATOR_2_OUT,Y:<DIG_IN,FW_ISR_LOCATOR_NOT_OUT 
+; if locator 3 not out goto FW_ISR_LOCATOR_NOT_OUT
+	JCLR	#DIG_IN_BIT_LOCATOR_3_OUT,Y:<DIG_IN,FW_ISR_LOCATOR_NOT_OUT 
+; all three locators are out at this point, goto next stage
+	BCLR    #ST_FW_LOCATORS_OUT,X:<STATUS		; We have finished moving locators out
+	BSET    #ST_FW_READING_PROXIMITY,X:<STATUS	; Start moving wheel/checking proximity sensors
+; reset move timeout FW_MOVE_TIMEOUT to zero
 	MOVE	X:<ZERO,X0				; X0 = 0
-	CMP	X0,A					; Is FW_POS_MOVE (A) zero (X0)?
-	JNE 	<FW_ISR_END				; If (FW_POS_MOVE != 0) goto end of ISR
-; finish move
-	BCLR    #ST_FW_MOVE,X:<STATUS			; Clear ST_FW_MOVE status bit.
-	BCLR    #FW0_OUTPUT_MOVE,Y:<DIG_OUT		; Clear filter wheel 0 MOVE bit in Y:DIG_OUT.
-	JMP 	<FW_ISR_END				; goto end of ISR
-FW_ISR_FWM_OUT_DETENT
-	JCLR	#ST_FW_IN_DETENT,X:<STATUS,FW_ISR_END 	; If the status detent bit is clear,
-							; goto end of ISR (no detent state change).
-	BCLR	#ST_FW_IN_DETENT,X:<STATUS		; Clear ST_FW_IN_DETENT status bit.
-;	JMP 	<FW_ISR_END				; goto end (not nessassary, next line will do same)
-; Filter wheel reset
-FW_ISR_FWR
-	JCLR    #ST_FW_RESET,X:<STATUS,FW_ISR_END 	; If the reset bit in status is clear, go to FW_ISR_END
-; wheel 0
-	; check detent
-	JCLR	#FW0_INPUT_DETENT,Y:<DIG_IN,FW_ISR_END 	; If the detent bit in Y:DIG_IN is clear,
-							; continue reset (go to end).
-							; If the bit is set the wheel is in a detent position.
-	; check home
-	JCLR	#FW0_INPUT_HOME,Y:<DIG_IN,FW_ISR_END ; If the home bit in Y:DIG_IN is clear,continue reset (go to end).
-							; If the bit is set the wheel is in the home position.
-	BCLR    #ST_FW_RESET,X:<STATUS			; Clear ST_FW_RESET status bit.
-	BCLR    #FW0_OUTPUT_MOVE,Y:<DIG_OUT		; Clear filter wheel MOVE bit in Y:DIG_OUT.
+	MOVE	X0,Y:<FW_MOVE_TIMEOUT			; FW_MOVE_TIMEOUT = 0
+	JMP	<FW_ISR_END
+FW_ISR_LOCATOR_NOT_OUT
+; FW_MOVE_TIMEOUT = (FW_MOVE_TIMEOUT+1)
+	MOVE	Y:<FW_MOVE_TIMEOUT,A			; A = FW_MOVE_TIMEOUT
+	MOVE	X:<ONE,X0				; X0 = 1
+	ADD	X0,A					; A = FW_MOVE_TIMEOUT+1
+	MOVE	A,Y:<FW_MOVE_TIMEOUT			; FW_MOVE_TIMEOUT = A (FW_MOVE_TIMEOUT+1)
+; if FW_MOVE_TIMEOUT >= #100000 move timeout
+	MOVE	#>100000,X0
+	CMP	X0,A					; Is FW_MOVE_TIMEOUT < #100000
+	JLT	<FW_ISR_END				; If (FW_MOVE_TIMEOUT < #100000) goto FW_ISR_END
+; timeout error
+	BCLR    #ST_FW_MOVE,X:<STATUS			; Take out of filter wheel move mode.
+	BCLR    #ST_FW_LOCATORS_OUT,X:<STATUS		; We are no longer moving the locators out
+	MOVE	#>6,X0					; X0 = 6
+	MOVE	X0,Y:<FW_ERROR_CODE			; Y:<FW_ERROR_CODE = X0 = 6
+	JMP	<FW_ISR_END
+FW_ISR_READING_PROXIMITY
+; If we are not moving the wheel and checking proximity sensors goto FW_ISR_LOCATORS_IN
+	JCLR	#ST_FW_READING_PROXIMITY,X:<STATUS,FW_ISR_LOCATORS_IN 
+	BSET	#DIG_OUT_BIT_MOTOR_ON,Y:<DIG_OUT	; Turn motor on
+; check proximity sensor pattern against target
+	MOVE	Y:<FW_TARGET_PROXIMITY_PATTERN,X0	; X0 = Y:<FW_TARGET_PROXIMITY_PATTERN
+	MOVE	Y:<DIG_IN,A				; A = Y:<DIG_IN
+	MOVE	#DIG_IN_PROXIMITY_MASK,X1		; X1 = #DIG_IN_PROXIMITY_MASK
+	AND	X1,A				; A = A & X1 (#DIG_IN_PROXIMITY_MASK), isolate DIG_IN proximity bits
+	CMP	X0,A					; Does X0 (FW_TARGET_PROXIMITY_PATTERN) == 
+							; A (DIG_IN & PROXIMITY_MASK)
+; If we are at the right proximity location, start stopping movement
+	JEQ	<FW_ISR_READING_PROXIMITY_STOP
+; FW_MOVE_TIMEOUT = (FW_MOVE_TIMEOUT+1)
+	MOVE	Y:<FW_MOVE_TIMEOUT,A			; A = FW_MOVE_TIMEOUT
+	MOVE	X:<ONE,X0				; X0 = 1
+	ADD	X0,A					; A = FW_MOVE_TIMEOUT+1
+	MOVE	A,Y:<FW_MOVE_TIMEOUT			; FW_MOVE_TIMEOUT = A (FW_MOVE_TIMEOUT+1)
+; if FW_MOVE_TIMEOUT >= #100000 move timeout
+	MOVE	#>100000,X0
+	CMP	X0,A					; Is FW_MOVE_TIMEOUT < #100000
+	JLT	<FW_ISR_END				; If (FW_MOVE_TIMEOUT < #100000) goto FW_ISR_END
+; timeout error
+	BCLR    #ST_FW_MOVE,X:<STATUS			; Take out of filter wheel move mode.
+	BCLR	#DIG_OUT_BIT_MOTOR_ON,Y:<DIG_OUT	; Turn motor off
+	BCLR    #ST_FW_READING_PROXIMITY,X:<STATUS	; We are no longer moving wheel/reading proximity sensors
+	MOVE	#>7,X0					; X0 = 7
+	MOVE	X0,Y:<FW_ERROR_CODE			; Y:<FW_ERROR_CODE = X0 = 7
+	JMP	<FW_ISR_END
+FW_ISR_READING_PROXIMITY_STOP
+	BCLR	#DIG_OUT_BIT_MOTOR_ON,Y:<DIG_OUT	; Turn motor off NOW rather than later
+	BCLR    #ST_FW_READING_PROXIMITY,X:<STATUS	; We have finished reading proximity sensors
+	BSET    #ST_FW_LOCATORS_IN,X:<STATUS		; Start moving locators in
+; reset move timeout FW_MOVE_TIMEOUT to zero
+	MOVE	X:<ZERO,X0				; X0 = 0
+	MOVE	X0,Y:<FW_MOVE_TIMEOUT			; FW_MOVE_TIMEOUT = 0
+	JMP	<FW_ISR_END
+FW_ISR_LOCATORS_IN
+; If we are not moving the locators in goto FW_ISR_DISENGAGE_CLUTCH
+	JCLR	#ST_FW_LOCATORS_IN,X:<STATUS,FW_ISR_DISENGAGE_CLUTCH
+; move locators in
+	BSET	#DIG_OUT_BIT_LOCATOR_1_IN,Y:<DIG_OUT	; move locator 1 in
+	BSET	#DIG_OUT_BIT_LOCATOR_2_IN,Y:<DIG_OUT	; move locator 2 in
+	BSET	#DIG_OUT_BIT_LOCATOR_3_IN,Y:<DIG_OUT	; move locator 3 in
+; check whether the locators are in
+; if locator 1 not in goto FW_ISR_LOCATOR_NOT_IN
+	JCLR	#DIG_IN_BIT_LOCATOR_1_IN,Y:<DIG_IN,FW_ISR_LOCATOR_NOT_IN
+; if locator 2 not in goto FW_ISR_LOCATOR_NOT_IN
+	JCLR	#DIG_IN_BIT_LOCATOR_2_IN,Y:<DIG_IN,FW_ISR_LOCATOR_NOT_IN 
+; if locator 3 not in goto FW_ISR_LOCATOR_NOT_IN
+	JCLR	#DIG_IN_BIT_LOCATOR_3_IN,Y:<DIG_IN,FW_ISR_LOCATOR_NOT_IN 
+; all three locators are in at this point, goto next stage
+	BCLR    #ST_FW_LOCATORS_IN,X:<STATUS		; We have finished moving locators in
+	BSET    #ST_FW_DISENGAGE_CLUTCH,X:<STATUS	; Start disengaging clutch
+; reset move timeout FW_MOVE_TIMEOUT to zero
+	MOVE	X:<ZERO,X0				; X0 = 0
+	MOVE	X0,Y:<FW_MOVE_TIMEOUT			; FW_MOVE_TIMEOUT = 0
+	JMP	<FW_ISR_END
+FW_ISR_LOCATOR_NOT_IN
+; FW_MOVE_TIMEOUT = (FW_MOVE_TIMEOUT+1)
+	MOVE	Y:<FW_MOVE_TIMEOUT,A			; A = FW_MOVE_TIMEOUT
+	MOVE	X:<ONE,X0				; X0 = 1
+	ADD	X0,A					; A = FW_MOVE_TIMEOUT+1
+	MOVE	A,Y:<FW_MOVE_TIMEOUT			; FW_MOVE_TIMEOUT = A (FW_MOVE_TIMEOUT+1)
+; if FW_MOVE_TIMEOUT >= #100000 move timeout
+	MOVE	#>100000,X0
+	CMP	X0,A					; Is FW_MOVE_TIMEOUT < #100000
+	JLT	<FW_ISR_END				; If (FW_MOVE_TIMEOUT < #100000) goto FW_ISR_END
+; timeout error
+	BCLR    #ST_FW_MOVE,X:<STATUS			; Take out of filter wheel move mode.
+	BCLR    #ST_FW_LOCATORS_IN,X:<STATUS		; We are no longer moving the locators in
+	MOVE	#>8,X0					; X0 = 8
+	MOVE	X0,Y:<FW_ERROR_CODE			; Y:<FW_ERROR_CODE = X0 = 8
+	JMP	<FW_ISR_END
+FW_ISR_DISENGAGE_CLUTCH
+; If we are not disengaging the clutch goto FW_ISR_END
+	JCLR	#ST_FW_DISENGAGE_CLUTCH,X:<STATUS,FW_ISR_END
+
+	BCLR	#DIG_OUT_BIT_CLUTCH_ENGAGE,Y:<DIG_OUT ; clear engage clutch on Y:DIG_OUT
+; Have we disengaged the clutch yet
+; if clutch disengaged goto FW_ISR_DISENGAGE_CLUTCH_DONE 
+	JSET	#DIG_IN_BIT_CLUTCH_DISENGAGED,Y:<DIG_IN,FW_ISR_DISENGAGE_CLUTCH_DONE
+; FW_MOVE_TIMEOUT = (FW_MOVE_TIMEOUT+1)
+	MOVE	Y:<FW_MOVE_TIMEOUT,A			; A = FW_MOVE_TIMEOUT
+	MOVE	X:<ONE,X0				; X0 = 1
+	ADD	X0,A					; A = FW_MOVE_TIMEOUT+1
+	MOVE	A,Y:<FW_MOVE_TIMEOUT			; FW_MOVE_TIMEOUT = A (FW_MOVE_TIMEOUT+1)
+; if FW_MOVE_TIMEOUT >= #100000 move timeout
+	MOVE	#>100000,X0
+	CMP	X0,A					; Is FW_MOVE_TIMEOUT < #100000
+	JLT	<FW_ISR_END				; If (FW_MOVE_TIMEOUT < #100000) goto FW_ISR_END
+; timeout error
+	BCLR    #ST_FW_MOVE,X:<STATUS			; Take out of filter wheel move mode.
+	BCLR    #ST_FW_DISENGAGE_CLUTCH,X:<STATUS	; We are no longer engaging the clutch
+	MOVE	#>9,X0					; X0 = 9
+	MOVE	X0,Y:<FW_ERROR_CODE			; Y:<FW_ERROR_CODE = X0 = 9
+	JMP	<FW_ISR_END
+FW_ISR_DISENGAGE_CLUTCH_DONE
+	BCLR    #ST_FW_DISENGAGE_CLUTCH,X:<STATUS	; We have finished disengaging the clutch
+; reset move timeout FW_MOVE_TIMEOUT to zero
+	MOVE	X:<ZERO,X0				; X0 = 0
+	MOVE	X0,Y:<FW_MOVE_TIMEOUT			; FW_MOVE_TIMEOUT = 0
+; We have finished moving the filter wheel, switch off the filter wheel move mode.
+	BCLR    #ST_FW_MOVE,X:<STATUS			; Take out of filter wheel move mode.
+; check proximity sensor pattern against target
+	MOVE	Y:<FW_TARGET_PROXIMITY_PATTERN,X0	; X0 = Y:<FW_TARGET_PROXIMITY_PATTERN
+	MOVE	Y:<DIG_IN,A				; A = Y:<DIG_IN
+	MOVE	#DIG_IN_PROXIMITY_MASK,X1		; X1 = #DIG_IN_PROXIMITY_MASK
+	AND	X1,A				; A = A & X1 (#DIG_IN_PROXIMITY_MASK), isolate DIG_IN proximity bits
+	CMP	X0,A					; Does X0 (FW_TARGET_PROXIMITY_PATTERN) == 
+							; A (DIG_IN & PROXIMITY_MASK)
+; If we are at the right proximity location, we have finished!
+	JEQ	<FW_ISR_END
+; We have stopped moving in the wrong position, set an error
+	MOVE	#>10,X0					; X0 = 10
+	MOVE	X0,Y:<FW_ERROR_CODE			; Y:<FW_ERROR_CODE = X0 = 10
+; end of filter wheel part of the ISR
 FW_ISR_END
 
        COMMENT * 
 	$Log: not supported by cvs2svn $
+	Revision 1.1  2011/06/02 14:06:08  cjm
+	Initial revision
+
 	*
