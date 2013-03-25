4bcc43f94f606270b7accf357fb557d18810fae0
Chris Mottram <C.Mottram@ljmu.ac.uk>
2013-03-25 15:07:14 +0000

    Added new constructor for format's with an illegal integer argument.

--- ioo/java/ngat/o/ccd/CCDLibraryFormatException.java
+++ ioo/java/ngat/o/ccd/CCDLibraryFormatException.java
@@ -1,19 +1,19 @@
 // CCDLibraryFormatException.java
-// $Header: /space/home/eng/cjm/cvs/ioo/java/ngat/o/ccd/CCDLibraryFormatException.java,v 1.1 2011-11-23 10:59:30 cjm Exp $
+// $Header: /space/home/eng/cjm/cvs/ioo/java/ngat/o/ccd/CCDLibraryFormatException.java,v 1.2 2013-03-25 15:07:13 cjm Exp $
 package ngat.o.ccd;
 
 /**
  * This class extends java.lang.IllegalArgumentException. Objects of this class are thrown when an illegal
  * format argument is passed into various parse routines in CCDLibrary.
  * @author Chris Mottram
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class CCDLibraryFormatException extends IllegalArgumentException
 {
 	/**
 	 * Revision Control System id string, showing the version of the Class.
 	 */
-	public final static String RCSID = new String("$Id: CCDLibraryFormatException.java,v 1.1 2011-11-23 10:59:30 cjm Exp $");
+	public final static String RCSID = new String("$Id: CCDLibraryFormatException.java,v 1.2 2013-03-25 15:07:13 cjm Exp $");
 
 	/**
 	 * Constructor for the exception.
@@ -26,8 +26,23 @@ public class CCDLibraryFormatException extends IllegalArgumentException
 		super("CCDLibraryFormatException:"+fromClassName+":"+methodName+":Illegal Parameter:"+
 			illegalParameter);
 	}
+
+	/**
+	 * Constructor for the exception.
+	 * @param fromClassName The name of the class the exception occured in.
+	 * @param methodName The name of the method the exception occured in.
+	 * @param illegalParameter The illegal integer that could not be parsed by the method.
+	 */
+	public CCDLibraryFormatException(String fromClassName,String methodName,int illegalParameter)
+	{
+		super("CCDLibraryFormatException:"+fromClassName+":"+methodName+":Illegal Value:"+
+			illegalParameter);
+	}
 }
 
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.1  2011/11/23 10:59:30  cjm
+// Initial revision
+//
 //
